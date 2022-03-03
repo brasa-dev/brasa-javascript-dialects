eae6c63f1f64fd807a05b08ae135ff3fc76dde11
marcel0ll <marcelo@lotuz.dev>
2022-03-02 23:12:29 -0300

    add en dialect

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ for dialect in ./dialects/*; do
   DIALECT=${dialect##./dialects/}
   echo $DIALECT
 
-  jq '. | {from: "en", to: .dialect, label: .label, version: .version, lexicon: (.keywords + .literals + .special) }' "$dialect/root.json" > ./build/to_$DIALECT.json
-  jq '. | {from: .dialect, to: "en", label: "javascript", version: .version, lexicon: (.keywords + .literals + .special | to_entries | map({(.value): .key}) | add) }' "$dialect/root.json" > build/from_$DIALECT.json
+  jq '. | {extension: .extension, dialect: .dialect, from: "en", to: .dialect, label: .label, version: .version, lexicon: (.keywords + .literals + .special) }' "$dialect/root.json" > ./build/to_$DIALECT.json
+  jq '. | {extension: .extension, dialect: "en", from: .dialect, to: "en", label: "javascript", version: .version, lexicon: (.keywords + .literals + .special | to_entries | map({(.value): .key}) | add) }' "$dialect/root.json" > build/from_$DIALECT.json
 done
 
