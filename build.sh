a518d1071cfb2254041c5a18dc25c3d71a8c0a11
marcel0ll <marcelo@lotuz.dev>
2023-07-11 17:08:01 -0300

    recursively build language map

--- build.sh
+++ build.sh
@@ -8,7 +8,10 @@ for dialect in ./dialects/*; do
   DIALECT=${dialect##./dialects/}
   echo $DIALECT
 
-  jq '. | {extension: .extension, dialect: .dialect, from: "en", to: .dialect, label: .label, version: .version, lexicon: (.keywords + .literals + .special) }' "$dialect/root.json" > ./build/to_$DIALECT.json
-  jq '. | {extension: .extension, dialect: "en", from: .dialect, to: "en", label: "javascript", version: .version, lexicon: (.keywords + .literals + .special | to_entries | map({(.value): .key}) | add) }' "$dialect/root.json" > build/from_$DIALECT.json
+  jq -s 'add' $( find $dialect -name '*.json' ) > /tmp/lang.json
+
+  jq '. | {extension: .extension, dialect: .dialect, from: "en", to: .dialect, label: .label, version: .version, identifiers: .identifiers, lexicon: (.keywords + .literals + .special) }' '/tmp/lang.json' > ./build/en_$DIALECT.json
+
+  jq '. | {extension: .extension, dialect: "en", from: .dialect, to: "en", label: "javascript", version: .version, identifiers: .identifiers | walk(if type == "object" and has("id") then if has("identifiers") then {id: .value, value: .id, identifiers: .identifiers} else {id: .value, value: .id} end else . end), lexicon: (.keywords + .literals + .special | to_entries | map({(.value): .key}) | add) }' '/tmp/lang.json' > "build/${DIALECT}_en".json
 done
 
